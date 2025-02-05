88b7c52af0a89bdce605020d5edcd31875cd168d
icy-chu <yilichu0618@163.com>
2025-02-05 20:31:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))  # 默认端口为 5001
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
